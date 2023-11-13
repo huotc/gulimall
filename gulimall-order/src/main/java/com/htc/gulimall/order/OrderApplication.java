ab58527b820569fa01f767a53d8fdc6442280a3f
huotengchao <huotengchao@lucidata.cn>
2023-11-13 17:21:45 +0800

    项目初始结构创建

---
+++ gulimall-order/src/main/java/com/htc/gulimall/order/OrderApplication.java
@@ -0,0 +1,7 @@
+package com.htc.gulimall.order;
+
+public class OrderApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ gulimall-ware/src/main/java/com/htc/gulimall/ware/WareApplication.java
@@ -0,0 +1,7 @@
+package com.htc.gulimall.ware;
+
+public class WareApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
