af7358e3cf5c7ca5bd262f893689db6c35efb1be
huotengchao <huotengchao@lucidata.cn>
2024-01-18 15:44:43 +0800

    项目初始结构创建

--- gulimall-coupon/src/main/java/com/htc/gulimall/coupon/CouponApplication.java
+++ gulimall-coupon/src/main/java/com/htc/gulimall/coupon/CouponApplication.java
@@ -1,7 +1,11 @@
 package com.htc.gulimall.coupon;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CouponApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(CouponApplication.class, args);
     }
 }
--- gulimall-order/src/main/java/com/htc/gulimall/order/OrderApplication.java
+++ gulimall-order/src/main/java/com/htc/gulimall/order/OrderApplication.java
@@ -1,7 +1,11 @@
 package com.htc.gulimall.order;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class OrderApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(OrderApplication.class, args);
     }
 }
