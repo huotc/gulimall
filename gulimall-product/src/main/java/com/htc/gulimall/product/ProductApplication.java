d8ee3946f5a30d71ae55672326f5ec9e6ebc95a7
huotc <ishuotc@163.com>
2025-07-21 17:46:28 +0800

    spring cache

--- gulimall-product/src/main/java/com/htc/gulimall/product/ProductApplication.java
+++ gulimall-product/src/main/java/com/htc/gulimall/product/ProductApplication.java
@@ -3,6 +3,7 @@ package com.htc.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -80,6 +81,31 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  *              MyRedissonConfig给容器中配置一个RedissonClient实例即可
  *      3）、使用
  *          参照文档做。
+ *
+ * 8、整合SpringCache简化缓存开发
+ *      1）、引入依赖
+ *          spring-boot-starter-cache、spring-boot-starter-data-redis
+ *      2）、写配置
+ *          （1）、自动配置了哪些
+ *              CacheAuroConfiguration会导入 RedisCacheConfiguration；
+ *              自动配好了缓存管理器RedisCacheManager
+ *          （2）、配置使用redis作为缓存
+ *              spring.cache.type=redis
+ *      3）、测试使用缓存
+ *          @Cacheable: Triggers cache population.：触发将数据保存到缓存的操作
+ *          @CacheEvict: Triggers cache eviction.：触发将数据从缓存删除的操作
+ *          @CachePut: Updates the cache without interfering with the method execution.：不影响方法执行更新缓存
+ *          @Caching: Regroups multiple cache operations to be applied on a method.：组合以上多个操作
+ *          @CacheConfig: Shares some common cache-related settings at class-level.：在类级别共享缓存的相同配置
+ *          1）、开启缓存功能 @EnableCaching
+ *          2）、只需要使用注解就能完成缓存操作
+ *
+ *      4）、原理：
+ *          CacheAutoConfiguration ->  RedisCacheConfiguration ->
+ *          自动配置了RedisCacheManager->初始化所有的缓存->每个缓存决定使用什么配置
+ *          ->如果redisCacheConfiguration有就用已有的，没有就用默认配置
+ *          ->想改缓存的配置，只需要给容器中放一个RedisCacheConfiguration即可
+ *          ->就会应用到当前RedisCacheManager管理的所有缓存分区中
  *
  *
  */
--- gulimall-product/src/main/java/com/htc/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/htc/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -17,6 +17,8 @@ import com.htc.gulimall.product.vo.Catelog2Vo;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
@@ -83,8 +85,25 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
                 .toArray(Long[]::new);
     }
 
+    /**
+     * 级联更新所有关联的数据
+     * @CacheEvict:失效模式
+     * 1、同时进行多种缓存操作  @Caching
+     * 2、指定删除某个分区下的所有数据 @CacheEvict(value = "category",allEntries = true)
+     * 3、存储同一类型的数据，都可以指定成同一个分区。分区名默认就是缓存的前缀
+     * @param category
+     */
+
+
+//    @Caching(evict = {
+//            @CacheEvict(value = "category",key = "'getLevel1Categorys'"),
+//            @CacheEvict(value = "category",key = "'getCatalogJson'")
+//    })
+    //category:key
+    @CacheEvict(value = "category",allEntries = true) //失效模式
+//    @CachePut //双写模式
+    @Transactional
     @Override
-    @Transactional(rollbackFor = Exception.class)
     public void updateCascade(CategoryEntity category) {
         this.updateById(category);
         if (StrUtil.isNotEmpty(category.getName())) {
@@ -96,7 +115,43 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         }
     }
     
-    
+    /**
+     * 1、每一个需要缓存的数据我们都来指定要放到那个名字的缓存。【缓存的分区(按照业务类型分)】
+     * 2、 @Cacheable({"category"})
+     *      代表当前方法的结果需要缓存，如果缓存中有，方法不用调用。
+     *      如果缓存中没有，会调用方法，最后将方法的结果放入缓存
+     * 3、默认行为
+     *      1）、如果缓存中有，方法不用调用。
+     *      2）、key默认自动生成；缓存的名字::SimpleKey [](自主生成的key值)
+     *      3）、缓存的value的值。默认使用jdk序列化机制，将序列化后的数据存到redis
+     *      4）、默认ttl时间 -1；
+     *
+     *    自定义：
+     *      1）、指定生成的缓存使用的key：  key属性指定，接受一个SpEL
+     *             SpEL的详细https://docs.spring.io/spring/docs/5.1.12.RELEASE/spring-framework-reference/integration.html#cache-spel-context
+     *      2）、指定缓存的数据的存活时间： 配置文件中修改ttl
+     *      3）、将数据保存为json格式:
+     *              自定义RedisCacheConfiguration即可
+     * 4、Spring-Cache的不足；
+     *      1）、读模式：
+     *          缓存穿透：查询一个null数据。解决：缓存空数据；ache-null-values=true
+     *          缓存击穿：大量并发进来同时查询一个正好过期的数据。解决：加锁；？默认是无加锁的;sync = true（加锁，解决击穿）
+     *          缓存雪崩：大量的key同时过期。解决：加随机时间。加上过期时间。：spring.cache.redis.time-to-live=3600000
+     *      2）、写模式：（缓存与数据库一致）
+     *          1）、读写加锁。
+     *          2）、引入Canal，感知到MySQL的更新去更新数据库
+     *          3）、读多写多，直接去数据库查询就行
+     *    总结：
+     *      常规数据（读多写少，即时性，一致性要求不高的数据）；完全可以使用Spring-Cache；写模式（只要缓存的数据有过期时间就足够了）
+     *      特殊数据：特殊设计
+     *
+     *   原理：
+     *      CacheManager(RedisCacheManager)->Cache(RedisCache)->Cache负责缓存的读写
+     *
+     * @return {@link List }<{@link CategoryEntity }>
+     * @author huotengchao
+     */
+    @Cacheable(value = {"category"},key = "#root.method.name",sync = true)
     @Override
     public List<CategoryEntity> getLevel1Categorys() {
         System.out.println("getLevel1Categorys.....");
@@ -105,8 +160,56 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return categoryEntities;
     }
     
+    
+    @Cacheable(value = "category",key = "#root.methodName")
     @Override
     public Map<String, List<Catelog2Vo>> getCatalogJson() {
+        System.out.println("查询了数据库.....");
+         List<CategoryEntity> selectList = baseMapper.selectList(null);
+        //1、查出所有1级分类
+        List<CategoryEntity> level1Categorys = getParent_cid(selectList, 0L);
+
+        //2、封装数据
+        Map<String, List<Catelog2Vo>> parent_cid = level1Categorys.stream().collect(Collectors.toMap(k -> k.getCatId().toString(), v -> {
+            //1、每一个的一级分类，查到这个一级分类的二级分类
+            List<CategoryEntity> categoryEntities = getParent_cid(selectList, v.getCatId());
+            //2、封装上面面的结果
+            List<Catelog2Vo> catelog2Vos = null;
+            if (categoryEntities != null) {
+                catelog2Vos = categoryEntities.stream().map(l2 -> {
+                    Catelog2Vo catelog2Vo = new Catelog2Vo(v.getCatId().toString(), null, l2.getCatId().toString(), l2.getName());
+                    //1、找当前二级分类的三级分类封装成vo
+                    List<CategoryEntity> level3Catelog = getParent_cid(selectList, l2.getCatId());
+                    if (level3Catelog != null) {
+                        List<Catelog2Vo.Catelog3Vo> collect = level3Catelog.stream().map(l3 -> {
+                            //2、封装成指定格式
+                            Catelog2Vo.Catelog3Vo catelog3Vo = new Catelog2Vo.Catelog3Vo(l2.getCatId().toString(), l3.getCatId().toString(), l3.getName());
+                            return catelog3Vo;
+                        }).collect(Collectors.toList());
+                        catelog2Vo.setCatalog3List(collect);
+                    }
+                    return catelog2Vo;
+                }).collect(Collectors.toList());
+            }
+
+
+            return catelog2Vos;
+        }));
+        
+        return parent_cid;
+    }
+    
+    
+    //TODO 产生堆外内存溢出：OutOfDirectMemoryError
+    //1）、springboot2.0以后默认使用lettuce作为操作redis的客户端。它使用netty进行网络通信。
+    //2）、lettuce的bug导致netty堆外内存溢出 -Xmx300m；netty如果没有指定堆外内存，默认使用-Xmx300m
+    //   可以通过-Dio.netty.maxDirectMemory进行设置
+    //解决方案：不能使用-Dio.netty.maxDirectMemory只去调大堆外内存。
+    //1）、升级lettuce客户端。   2）、切换使用jedis
+    //  redisTemplate：
+    //  lettuce、jedis操作redis的底层客户端。Spring再次封装redisTemplate；
+   // @Override
+    public Map<String, List<Catelog2Vo>> getCatalogJson2() {
         //给缓存中放json字符串，拿出的json字符串，还用逆转为能用的对象类型；【序列化与反序列化】
         /**
          * 1、空结果缓存：解决缓存穿透
